8b402e9a41620da948b96386a3bbe995b9639b07
wevexOP <111082615+wevexOP@users.noreply.github.com>
2023-03-08 15:32:50 -0800

    adding most of the main files to the repo

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -21,14 +19,17 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Webpack plugin that generates our html file and injects our bundles
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'JATE'
       }),
+      // Injects our custom servie worker
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
+      // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
@@ -51,6 +52,7 @@ module.exports = () => {
     ],
 
     module: {
+      // CSS Loaders
       rules: [
         {
           test: /\.css$/i,
@@ -59,6 +61,7 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
@@ -71,4 +74,3 @@ module.exports = () => {
     },
   };
 };
-
